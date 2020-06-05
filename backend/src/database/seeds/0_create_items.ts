9213af379c19af5843c5813f2129e0ec916a0587
Antony Santos <antonyc828@gmail.com>
2020-06-05 11:38:59 -0300

    fix seeds file

--- backend/src/database/seeds/0_create_items.ts
+++ backend/src/database/seeds/0_create_items.ts
@@ -8,6 +8,6 @@ export async function seed(knex: Knex) {
     { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos ', image: 'organicos.svg' },
-    { title: 'Óleo de Cozinha', image: 'ole.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
   ] as IItem[]);
 }
