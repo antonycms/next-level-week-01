4e208571b837bc1c7b164b605e74f8b601517f8a
Antony Santos <antonyc828@gmail.com>
2020-06-05 02:17:24 -0300

    added map in create point page

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -43,6 +44,14 @@ const CreatePoint: React.FC = () => {
             <legend>Endereco</legend>
             <span>Selecione o endereco no mapa</span>
           </div>
+
+          <Map center={[-10.1246393, -36.1653911]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </Map>
+
           <div className="col-container">
             <div className="col col-4">
               <label htmlFor="name">Estado (UF)</label>
@@ -61,6 +70,40 @@ const CreatePoint: React.FC = () => {
 
           <label htmlFor="name">Nome da entidade</label>
           <input className="col-10" type="text" placeholder="ex: entidade" />
+        </fieldset>
+
+        <fieldset>
+          <div className="space-betwen">
+            <legend>Ítens de coleta</legend>
+            <span>Selecione um ou mais itens abaixo</span>
+          </div>
+
+          <ul className="point-items">
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+            <li>
+              <img src="http://192.168.100.53:3003/uploads/lampadas.svg" alt="lampada" />
+              <span>Lampada</span>
+            </li>
+          </ul>
         </fieldset>
       </form>
     </div>
